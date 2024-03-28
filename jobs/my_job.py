5fea1132687561ad703fdf7c0084d6ef267afb0b
BartoszKuzimski <163850332+BartoszKuzimski@users.noreply.github.com>
2024-03-28 12:26:06 +0100

    correct_job

--- jobs/my_job.py
+++ jobs/my_job.py
@@ -4,9 +4,11 @@ class SimpleLogJob(Job):
     class Meta:
         name = "Simple Log Message"
         description = "Logs a simple message to the job output."
-        message = StringVar(
-            description="This is me doing code "
-            )
+
+    message = StringVar(
+        description="The message to log."
+    )
+
     def run(self,**kwargs):
         self.logger.info(str(kwargs))
         return kwargs
